f56b5b5facc013bd4e7f8ce1a0e4418068b3c01d
danield33 <75221181+danield33@users.noreply.github.com>
2020-12-14 08:30:33 -0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LINQListColdWeather {
+    class Weather {
+
+        public DayOfWeek Day{ get; set; }
+        public int HighTemp { get; set; }
+        public int LowTemp { get; set; }
+
+        public Weather(DayOfWeek day, int high, int low) {
+            this.Day = day;
+            this.HighTemp = high;
+            this.LowTemp = low;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace LINQListColdWeather {
+    class Program {
+
+        static void Main(string[] args) {
+
+            List<Weather> daysForcasted = new List<Weather>();
+            daysForcasted.Add(new Weather(DayOfWeek.Tue, 32, 26));
+            daysForcasted.Add(new Weather(DayOfWeek.Wed, 37, 28));
+            daysForcasted.Add(new Weather(DayOfWeek.Thu, 37, 29));
+            daysForcasted.Add(new Weather(DayOfWeek.Fri, 39, 29));
+            daysForcasted.Add(new Weather(DayOfWeek.Sat, 42, 34));
+
+            var coldDays = from day in daysForcasted
+                           where day.HighTemp < 40
+                           select day;
+
+            foreach(var day in coldDays) {
+                Console.WriteLine("Cold weather on " + day.Day);
+            }
+
+        }
+    }
+
+    public enum DayOfWeek {
+        Sun = 0,
+        Mon = 1,
+        Tue,
+        Wed,
+        Thu,
+        Fri,
+        Sat
+    }
+
+}
